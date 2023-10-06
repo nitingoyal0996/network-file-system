453481f5fa436cb5f567b94d32a7b73058bff63a
Nitin Goyal <goyalnitin634@gmail.com>
2023-10-05 23:07:29 -0400

    add definition for Symlink() function

--- absolutepath.py
+++ absolutepath.py
@@ -46,7 +46,7 @@ class AbsolutePathName():
   def Link(self, target, name, cwd):
 
     #  validate whether the file exist - 
-    target_inode_number = self.PathToInodeNumber(target, cwd)
+    target_inode_number = self.GeneralPathToInodeNumber(target, cwd)
     if target_inode_number == -1:
       logging.debug("ERROR_LINK_TARGET_DOESNOT_EXIST " + str(target))
       return -1, "ERROR_LINK_TARGET_DOESNOT_EXIST"
@@ -82,6 +82,116 @@ class AbsolutePathName():
     self.FileNameObject.InsertFilenameInodeNumber(cwd_inode, name, target_inode_number)
 
     # Increase Reference count for the source file Inode by one
+    target_inode.inode.refcnt = target_inode.inode.refcnt + 1
+    target_inode.StoreInode(self.FileNameObject.RawBlocks)
+
+    return 0, 'SUCCESS'
+
+  def Symlink (self, target, name, cwd):
+    # print(">>>> run symlink")
+
+    # ensure we have a valid target 
+    target_inode_number = self.GeneralPathToInodeNumber(target, cwd)
+    if target_inode_number == -1:
+      logging.debug("ERROR_SYMLINK_TARGET_DOESNOT_EXIST " + str(target))
+      return -1, "ERROR_SYMLINK_TARGET_DOESNOT_EXIST"
+    
+    target_inode = InodeNumber(target_inode_number)
+    target_inode.InodeNumberToInode(self.FileNameObject.RawBlocks)
+
+    cwd_inode = InodeNumber(cwd)
+    cwd_inode.InodeNumberToInode(self.FileNameObject.RawBlocks)
+    if cwd_inode.inode.type != fsconfig.INODE_TYPE_DIR:
+      logging.debug('ERROR_SYMLINK_NOT_DIRECTORY', cwd)
+      return -1, "ERROR_SYMLINK_NOT_DIRECTORY"
+
+    fileentry_position = self.FileNameObject.FindAvailableFileEntry(cwd)
+    if fileentry_position == -1:
+      logging.debug('ERROR_SYMLINK_DATA_BLOCK_NOT_AVAILABLE')
+      return -1, "ERROR_SYMLINK_DATA_BLOCK_NOT_AVAILABLE"
+
+    # validate for duplicate symlink
+    if self.FileNameObject.Lookup(name, cwd) != -1:
+      logging.debug('ERROR_SYMLINK_ALREADY_EXISTS' + str(target))
+      return -1, "ERROR_SYMLINK_ALREADY_EXISTS"
+    
+    # ERROR_SYMLINK_INODE_NOT_AVAILABLE
+    # Find if there is an available inode
+    inode_position = self.FileNameObject.FindAvailableInode()
+    if inode_position == -1:
+        logging.debug("ERROR_SYMLINK_INODE_NOT_AVAILABLE")
+        return -1, "ERROR_SYMLINK_INODE_NOT_AVAILABLE"
+      
+    new_inode = InodeNumber(inode_position)
+    new_inode.InodeNumberToInode(self.FileNameObject.RawBlocks)
+
+    file_path_byte_array = bytearray(target, "utf-8")
+    inode_offset = new_inode.inode.size
+    
+    if inode_offset + len(file_path_byte_array) > fsconfig.MAX_FILE_SIZE:
+      logging.debug("ERROR_SYMLINK_TARGET_EXCEEDS_BLOCK_SIZE. Available max: " + str(fsconfig.MAX_FILE_SIZE) + ", path size: " + str(file_path_byte_array))
+      return -1, "ERROR_SYMLINK_TARGET_EXCEEDS_BLOCK_SIZE"
+    
+    # update symlink inode configurations - 
+    new_inode.inode.type = fsconfig.INODE_TYPE_SYM
+    new_inode.inode.refcnt = 1
+
+    num_of_blocks_required = len(file_path_byte_array) // fsconfig.BLOCK_SIZE;
+    # print('num of blocks required: ', num_of_blocks_required)
+
+    bytes_written = 0
+    current_offset = inode_offset
+    data = file_path_byte_array
+    # the data to be written may span multiple blocks
+    # this loop iterates through one or more blocks, ending when all data is written
+    while bytes_written < len(data):
+        # determine block index corresponding to the current offset where the write should take place
+        current_block_index = current_offset // fsconfig.BLOCK_SIZE
+
+        # determine the next block's boundary (in Bytes relative to the file's offset 0)
+        next_block_boundary = (current_block_index + 1) * fsconfig.BLOCK_SIZE
+        write_start = current_offset % fsconfig.BLOCK_SIZE
+        if (inode_offset + len(data)) >= next_block_boundary:
+            write_end = fsconfig.BLOCK_SIZE
+        else:
+            # otherwise, the data is truncated within this block
+            write_end = (inode_offset + len(data)) % fsconfig.BLOCK_SIZE
+
+        logging.debug('AbsolutePathName::Create Symlink: write_start: ' + str(write_start) + ' , write_end: ' + str(write_end))
+
+        # retrieve index of block to be written from inode's list
+        block_number = new_inode.inode.block_numbers[current_block_index]
+
+        # if the data block to be written is not allocated (i.e. the block_numbers list in the inode is zero at
+        # current_block_index), we need to allocate it
+        if block_number == 0:
+            # allocate new data block
+            new_block = self.FileNameObject.AllocateDataBlock()
+            # update inode's block number list (it will be written to raw storage before the method returns)
+            new_inode.inode.block_numbers[current_block_index] = new_block
+            block_number = new_block
+
+        # load existing block data from the disk
+        block = self.FileNameObject.RawBlocks.Get(block_number)
+        # copy slice of data into the right position in this block
+        block[write_start:write_end] = data[bytes_written:bytes_written + (write_end - write_start)]
+        # write modified block back to disk
+        self.FileNameObject.RawBlocks.Put(block_number, block)
+        # update offset, bytes written
+        current_offset += write_end - write_start
+        bytes_written += write_end - write_start
+
+        logging.debug('AbsolutePathName::Create Symlink: current_offset: ' + str(current_offset) + ' , bytes_written: ' + str(
+            bytes_written) + ' , len(data): ' + str(len(data)))
+
+    # save symlink inode information
+    new_inode.size = inode_offset + bytes_written
+    new_inode.StoreInode(self.FileNameObject.RawBlocks)
+
+    # Insert file name information from source file inode to available entry
+    self.FileNameObject.InsertFilenameInodeNumber(cwd_inode, name, new_inode.inode_number)
+
+    # update target inode information
     target_inode.inode.refcnt = target_inode.inode.refcnt + 1
     target_inode.StoreInode(self.FileNameObject.RawBlocks)
 
--- assignment-submissions/HW3.design.md
+++ assignment-submissions/HW3.design.md
@@ -17,3 +17,32 @@ To acomplish this function should be able to -
 1. After validation, all we need is - a. inode number of the file (target) we want, b. cwd inode number and then call FileNameObject.InsertFilenameInodeNumber() to insert the new (filename-inode) entry into the directory inode table
 
 2. Finally, we need to increase the reference count for the target file Inode by one.
+
+## _SYMLINK()_: Soft Link
+
+Symbolic links unlike hard links, creates a pointer to the original data directory/file. (they do not even have the same permissions as the original directory/file)
+
+We need a type for the symlink file as 'INODE_TYPE_SYM' as given in the file system.
+
+a. find and create a new inode to hold information about the symlink. - FindAvailableInode()
+b. validate whether the total available block_size is sufficient to store the original file path. - replicate checks on the bounds like Write operation. (inode offset + len(original_file_path) < fsconfig.MAX_FILE_SIZE)
+c. allocate an available and required number of blocks to store the original file path. and append the allocated blocks to the inode.block_numbers[] array.
+d. write the original file path to the allocated blocks. and increment the reference count for the symlink inode.
+e. save the symlink inode information into blocks - StoreInode()
+f. save the inode <-> symlink_name mapping into the directory table - InsertFilenameInodeNumber()
+g. we now increament the directory inode reference count with 1 and save this information. - StoreInode()
+e. _Note_: we don't need to increment the value for the original file since the symlink is not referencing but rather pointing towards this.
+
+Along with defining the _symlink()_ function we also need to modify few of the _shell.py_ commands.
+
+## _shell.py_ > ls
+
+ls command to indicate symlinks.
+
+Check if the inode.type == SYM, if true - retrieve data from the blocks for the file_path and create an array to log into the terminal - 
+
+@linkName -> /path/to/target/file
+
+## _shell.py_ > mirror, slice, cat, cd:
+
+we need to update the default `FileNameOperation.FileNameObject.Lookup` to `AbsolutePathNameObject.GeneralPathToInodeNumber`
